8355caf87bd5ba0fed2ee7bc4ef5980c5d0a3e45
nguye <nguyenvuong0308@gmail.com>
2026-07-14 14:11:49 +0700

    Add config tutorial ads

--- app/src/main/java/com/codebasetemplate/required/ads/AppAdPlaceName.kt
+++ app/src/main/java/com/codebasetemplate/required/ads/AppAdPlaceName.kt
@@ -18,6 +18,24 @@ sealed class AppAdPlaceName(override val name: String): IAdPlaceName {
     object FULLSCREEN_TEST_LAZY_LOAD : AppAdPlaceName("fullscreen_test_lazy_load")
     object REWARD_TEST : AppAdPlaceName("reward_test")
 
+
+    object ACTION_NEXT_IN_INTRODUCTION : AppAdPlaceName("action_next_in_introduction")
+    object ACTION_SKIP_IN_INTRODUCTION : AppAdPlaceName("action_skip_in_introduction")
+
+    object ANCHORED_ONBOARDING_BOTTOM : AppAdPlaceName("anchored_onboarding_bottom")
+    object ANCHORED_ONBOARDING_BOTTOM_v2 : AppAdPlaceName("anchored_onboarding_bottom_v2")
+    object ANCHORED_FULL_ONBOARDING : AppAdPlaceName("anchored_full_onboarding")
+    object ANCHORED_FULL_ONBOARDING_v2 : AppAdPlaceName("anchored_full_onboarding_v2")
+
+    object ANCHORED_CHANGE_LANGUAGE_BOTTOM : AppAdPlaceName("anchored_change_language_bottom")
+    object ANCHORED_UNINSTALL_BOTTOM_STEP_1 : AppAdPlaceName("anchored_uninstall_bottom_step_1")
+    object ANCHORED_UNINSTALL_BOTTOM_STEP_2 : AppAdPlaceName("anchored_uninstall_bottom_step_2")
+
+    object ACTION_OPEN_APP_FIRST_OPEN : AppAdPlaceName("action_app_open_first_open")
+    object ACTION_OPEN_APP : AppAdPlaceName("action_app_open")
+    object APP_OPEN_FIRST_OPEN : AppAdPlaceName("open_app_first_open")
+    object APP_OPEN : AppAdPlaceName("open_app")
+
     companion object {
         /**Cần add thêm vào đây nếu tạo thêm AdPlaceName*/
         val APP_AD_PLACE_LIST: List<AppAdPlaceName> by lazy {
